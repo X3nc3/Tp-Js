694ff74618b9a01b297e793920c68f1679c2b2b1
Maxence <maxence.ogier@gmail.com>
2024-12-10 12:32:27 +0100

    exo 1, 2, 3, 4

--- exercice4.js
+++ exercice4.js
@@ -1,10 +1,10 @@
-const carat = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+const caract = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
 let mdp = "";
 
 for (let i = 0; i < 10; i++) {
-  let index = Math.floor(Math.random() * carat.length);
-  mdp += carat[index];
+  let index = Math.floor(Math.random() * caract.length);
+  mdp += caract[index];
 }
 
 console.log(`Mot de passe aléatoire de 10 caractére : ${mdp}`);
